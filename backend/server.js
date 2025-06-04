acfc0cdc116aae76ef5726f82cb5a5aea4819c4c
Julia Mari Bernaus <jumaber@gmail.com>
2025-06-04 15:56:13 +0200

    updated cors url permissions

--- backend/server.js
+++ backend/server.js
@@ -19,6 +19,7 @@ app.use(
       "http://localhost:5173",
       "http://localhost:5174",
       "https://portfolio-2025-wyed.onrender.com",
+      "https://juliamaribernaus.netlify.app"
     ],
   })
 );
