f576c03f1bb99159f97d25ff45c77758891de5ae
Julia Mari Bernaus <jumaber@gmail.com>
2025-06-13 11:10:09 +0200

    minor changes

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { Routes, Route } from 'react-router-dom';
 import { Home } from './pages/Home';
 import { SinglePage } from "./pages/SinglePage";
 import { ImprintPage } from './pages/ImprintPage';
+import { PageNotFound } from './pages/PageNotFound';
 
 
 function App() {
@@ -12,6 +13,7 @@ function App() {
         <Route path="/" element={<Home />} />
         <Route path="/project/:slug" element={<SinglePage />} />
         <Route path="/imprint" element={<ImprintPage />} />
+        <Route path="/*" element={<PageNotFound />} />
       </Routes>
     </>
   );
